0f52c3a03e7898896aa60427273802c9b8a4390e
Kenet K Ortiz <42982324+KOrtizLedezma@users.noreply.github.com>
2023-02-23 21:18:50 +0000

    Input and Output changed

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.println("Enter a word:");
-    boolean state = false;
-    while(!state){
-      try {
-        String word = in.nextLine();
-        state = true;
-        int a = word.length();
-        System.out.println("The length of the word is: " + a);
-		  } catch (InputMismatchException e) {
-		      System.out.println("Input error, try again");
-		  }
-      in.nextLine();
-    }
+    String word = in.nextLine();
+    int a = word.length();
+    System.out.println("The length of the word is: " + a);
     
     
   }
